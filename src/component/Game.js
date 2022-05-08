3890129e2e4d71a1e0519a7c306a2cac6a19ab08
ShayFae <ifrahnoor108@gmail.com>
2022-05-08 04:13:53 -0400

    Add onClick test

--- src/component/Game.js
+++ src/component/Game.js
@@ -3,17 +3,18 @@ import './Game.css'
 
 export default function Game() {
 const meep = () => {
-   return <div className="container">{React.createElement('div', {class: 'test'})}
-   {React.createElement('div', {class: 'test'})}
-   {React.createElement('div', {class: 'test'})}
+   return <div className="container">
+   {React.createElement('div', {class: 'test', onClick: () => {alert('Hello')}})}   
+   {React.createElement('div', {class: 'test', onClick: () => {alert('Hello')}})}
+   {React.createElement('div', {class: 'test', onClick: () => {alert('Hello')}})}
 
-   {React.createElement('div', {class: 'test'})}
-   {React.createElement('div', {class: 'test'})}
-   {React.createElement('div', {class: 'test'})}
+   {React.createElement('div', {class: 'test', onClick: () => {alert('Hello')}})}
+   {React.createElement('div', {class: 'test', onClick: () => {alert('Hello')}})}
+   {React.createElement('div', {class: 'test', onClick: () => {alert('Hello')}})}
 
-   {React.createElement('div', {class: 'test'})}
-   {React.createElement('div', {class: 'test'})}
-   {React.createElement('div', {class: 'test'})}
+   {React.createElement('div', {class: 'test', onClick: () => {alert('Hello')}})}
+   {React.createElement('div', {class: 'test', onClick: () => {alert('Hello')}})}
+   {React.createElement('div', {class: 'test', onClick: () => {alert('Hello')}})}
    </div>
 
 } 
